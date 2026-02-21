db23c7123722ee35f024623cb52e437fb224b913
Tobias Hort <mail@tobiashort.com>
2026-02-21 22:09:51 +0100

    allow multiple arguments

--- example/example.go
+++ example/example.go
@@ -4,16 +4,16 @@ import "github.com/tobiashort/clog-go"
 
 func main() {
 	clog.Level = clog.LevelDebug
-	clog.Debug("Hey")
+	clog.Debug("Hey", "world")
 	clog.Debugf("Hey %s", "you")
 	clog.Debugs("What", "foo", true, "bar", true)
-	clog.Info("Hey")
+	clog.Info("Hey", "world")
 	clog.Infof("Hey %s", "you")
 	clog.Infos("What", "foo", true, "bar", true)
-	clog.Warn("Hey")
+	clog.Warn("Hey", "world")
 	clog.Warnf("Hey %s", "you")
 	clog.Warns("What", "foo", true, "bar", true)
-	clog.Error("Hey")
+	clog.Error("Hey", "world")
 	clog.Errorf("Hey %s", "you")
 	clog.Errors("What", "foo", true, "bar", true)
 }
